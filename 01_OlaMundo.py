225cf6995925af8ec097e36214049e6cde9fa5f3
Wildson Lab <wildson.lab@gmail.com>
2021-12-04 22:59:45 -0300

    Primeiro commit

---
+++ 01_OlaMundo.py
@@ -0,0 +1,4 @@
+# 01 - Ola Mundo!
+# Primeiro programa em Python
+
+print("Ola mundo!")
---
+++ 04_Operadores_Comparacao.py
@@ -0,0 +1,12 @@
+# 04 - Operadores Aritmeticos
+
+a = 3
+b = 2
+c = 5
+
+if a > b and a > c:
+    print("Maior numero: ".format(a))
+elif b > a and b > c:
+    print("Maior numero: ".format(b))
+else:
+    print("Maior numero: ".format(b))
