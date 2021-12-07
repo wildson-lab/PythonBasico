483023d43e1ee99e8bedb5a2c736e750612e27d8
Wildson Lab <wildson.lab@gmail.com>
2021-12-06 22:50:41 -0300

    git push origin main

---
+++ Conjuntos/01 - Conjuntos (basico).py
@@ -0,0 +1,21 @@
+# 01 - Conjuntos (Parte 1)
+# Este codigo demonstra a aplicacao de funcoes basicas de conjuntos em Python
+# Conjunto e um tipo que nao aceita repeticao de elementos.
+
+# Cria um novo conjunto contendo numeros (um conjunto pode conter diversos tipos)
+conjunto = {1, 2, 3, 4, 99, 5}
+
+# Imprime o tipo "conjunto"
+print(type(conjunto))
+
+# Imprime os elementos de um conjunto
+print(conjunto)
+
+# Adiciona o numero 6 ao conjunto
+conjunto.add(6)
+
+# Remove o numero 99 do conjunto
+conjunto.discard(99)
+
+# Imprime o conjunto apos as alteracoes
+print(conjunto)
---
+++ Conjuntos/02 - Operacoes entre conjuntos.py
@@ -0,0 +1,16 @@
+# 02 - Conjuntos (Parte 2)
+# Este codigo demonstra a aplicacao de operacoes entre conjuntos
+
+# Inicializa os conjuntos
+conjunto_1 = {1, 2, 3, 4, 5}
+conjunto_2 = {4, 5, 6, 7, 8}
+
+# Uniao entre conjuntos:
+conjunto_uniao = conjunto_1.union(conjunto_2)
+print(conjunto_uniao)
+
+# Intersecao entre conjuntos:
+conjunto_intersecao = conjunto_1.intersection(conjunto_2)
+print(conjunto_intersecao)
+
+# Diferenca do conjunto 1 para o conjunto 2
