15fc17550b3c725d730e306f00eb6e480d44509a
Wildson Lab <wildson.lab@gmail.com>
2022-06-19 19:18:42 -0300

    Update "Operadores"

---
+++ Operadores/Operadores_Comparacao.py
@@ -0,0 +1,11 @@
+# Operadores Comparacao
+
+a = int(input("Digite o valor de A: "))
+b = int(input("Digite o valor de B: "))
+
+if a > b:
+    print("A é maior que B")
+elif a == b:
+    print("A é igual a B")
+else:
+    print("A é menor que B")
---
+++ Operadores/Operadores_Matematicos.py
@@ -0,0 +1,25 @@
+# Este codigo apresenta operacoes matematicas basicas realizadas em Python.
+
+# Definicao de variaveis
+a = 5
+b = 3
+
+# Operações matemáticas
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+divInteira = a // b
+resto = a % b
+expoente = a ** b
+
+# Imprimindo os resultados
+print(f"a = {a}")
+print(f"b = {b}")
+print(f"Soma: {soma}")
+print(f"Subtracao: {subtracao}")
+print(f"Multiplicacao: {multiplicacao}")
+print(f"Divisao: {divisao}")
+print(f"Divisao inteira: {divInteira}")
+print(f"Resto: {resto}")
+print(f"Expoente: {expoente}")
